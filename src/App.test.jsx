bbab028243f429c69f1bc2993ce2e7c843005e56
John <johnlouisstephenson@gmail.com>
2025-07-01 10:56:59 +0100

    test: add reset game test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,8 +14,7 @@ describe("App", () => {
   it("renders correctly", () => {
     expect(screen.getByText(/Next Player: X/i)).toBeInTheDocument();
     expect(screen.getByRole("button", { name: /Reset/i })).toBeInTheDocument();
-
-    expect(squares.length).toBe(9);
+    expect(squares).toHaveLength(9);
   });
 
   it("places an X correctly on first click", () => {
@@ -26,8 +25,20 @@ describe("App", () => {
     expect(screen.getByText(/Next Player: O/i)).toBeInTheDocument();
   });
 
+  it("doesn't change player mark if square clicked twice", () => {
+    fireEvent.click(squares[0]);
+
+    expect(squares[0]).toHaveTextContent(/X/);
+
+    fireEvent.click(squares[0]);
+
+    expect(squares[0]).toHaveTextContent(/X/);
+  });
+
   it("correctly identifies player X win", () => {
-    [0, 3, 1, 4, 2].forEach((index) => {
+    const xWinSequence = [0, 3, 1, 4, 2];
+
+    xWinSequence.forEach((index) => {
       fireEvent.click(squares[index]);
     });
 
@@ -36,5 +47,27 @@ describe("App", () => {
     fireEvent.click(squares[5]);
 
     expect(screen.getByText(/Winner: X/i)).toBeInTheDocument();
+  });
+
+  it("correctly identifies draw", () => {
+    const drawSequence = [0, 1, 2, 3, 5, 4, 6, 8, 7];
+
+    drawSequence.forEach((index) => {
+      fireEvent.click(squares[index]);
+    });
+
+    expect(screen.getByText(/Game Over/i)).toBeInTheDocument();
+  });
+
+  it("correctly resets the board", () => {
+    const resetButton = screen.getByRole("button", { name: /Reset/i });
+
+    [0, 1, 2].forEach((index) => {
+      fireEvent.click(squares[index]);
+    });
+
+    fireEvent.click(resetButton);
+
+    expect(screen.getAllByLabelText(/Empty/i)).toHaveLength(9);
   });
 });
